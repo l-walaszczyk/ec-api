8a5b77b0c11cf651e4caba5e3055b484c11fd25b
Łukasz Walaszczyk <l.walaszczyk@interia.pl>
2020-07-30 00:01:36 +0200

    (hopefully) final fix of the bug with time offset

--- functions/generateWeekArray.js
+++ functions/generateWeekArray.js
@@ -1,7 +1,7 @@
 const moment = require("moment-timezone");
 const generateHours = require("./generateHours");
 const timeZoneName = "Europe/Warsaw";
-// const timeZone = moment.tz.zone(timeZoneName);
+const timeZone = moment.tz.zone(timeZoneName);
 
 const generateWeekArray = (
   date,
@@ -12,19 +12,35 @@ const generateWeekArray = (
   ruleOverrides,
   meetings
 ) => {
+  const timeOffset = timeZone.parse(date);
+  const timeOffsetHours = timeOffset / 60;
+
   const weeksFirstDay = date.clone().startOf("isoWeek");
+
   const weekArray = [];
+
   for (let index = 0; index <= 6; index++) {
     const day = weeksFirstDay.clone().add(index, "day");
+
     let hours;
     if (
       (moment
         .utc()
+        .subtract(timeOffsetHours, "hour")
         .add(limitingHourHowManyDaysBefore, "day")
         .isSame(day.utc(), "day") &&
         moment.tz(timeZoneName).hour() >= limitingHourLocalTime) ||
-      day.utc().isAfter(moment.utc().add(1, "month").endOf("month"), "day") ||
-      day.utc().isBefore(moment.utc())
+      day
+        .utc()
+        .isAfter(
+          moment
+            .utc()
+            .subtract(timeOffsetHours, "hour")
+            .add(1, "month")
+            .endOf("month"),
+          "day"
+        ) ||
+      day.utc().isBefore(moment.utc().subtract(timeOffsetHours, "hour"))
     ) {
       hours = [];
     } else {
