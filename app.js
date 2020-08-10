f7085214d0fcbac0ec8fc7dc2901c835e9c05400
Łukasz Walaszczyk <l.walaszczyk@interia.pl>
2020-08-10 21:24:44 +0200

    add urlencoded @app.use

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ const app = express();
 
 // app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use((req, res, next) => {
   if (config.allowedOrigins.indexOf(req.headers.origin) > -1) {
